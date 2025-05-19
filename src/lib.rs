563712ddaaa697b0fce99148cf37083a9ec66bcc
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-05-19 21:24:45 +0200

    Added #[no_std]

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! # Iterator over characters with normalized line endings
 
+#![no_std]
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::missing_crate_level_docs)]
